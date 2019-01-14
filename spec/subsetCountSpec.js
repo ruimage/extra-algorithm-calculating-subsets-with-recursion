f2cb2fdf3f527c169b569a7733d1cd18772a2988
GlaticuS <glaticus@mail.ru>
2019-01-14 23:27:15 +0900

    some fixes

--- spec/subsetCountSpec.js
+++ spec/subsetCountSpec.js
@@ -1,14 +1,14 @@
 describe("calculating the number of subsets", () => {
 
-    it('zero subsets if zero options to choose from', async () => {
+    it('zero subsets if zero options to choose from', () => {
         expect(subsetCount(0, 5)).toEqual(0);
     });
 
-    it('one subset of size zero', async () => {
+    it('one subset of size zero', () => {
         expect(subsetCount(0, 5)).toEqual(1);
     });
 
-    it('number of size-one subsets is equal to the number of options', async () => {
+    it('number of size-one subsets is equal to the number of options', () => {
         expect(subsetCount(18, 1)).toEqual(18);
     });
 });
